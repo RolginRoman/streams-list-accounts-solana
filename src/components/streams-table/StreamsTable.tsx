c35dfd146ef4bb699c7e65d05a0f79501d771a65
roman.rolgin <irolgin@gmail.com>
2023-10-15 18:16:02 +0400

    feat: streams table col width

--- src/components/streams-table/StreamsTable.tsx
+++ src/components/streams-table/StreamsTable.tsx
@@ -14,7 +14,7 @@ export const StreamsTable = ({ streams }: Props) => {
           <TableHeadCell className="w-1/12">Key</TableHeadCell>
           <TableHeadCell className="w-4/12">Name</TableHeadCell>
           <TableHeadCell className="w-2/12">Amount</TableHeadCell>
-          <TableHeadCell className="w-3/12">Token</TableHeadCell>
+          <TableHeadCell className="w-2/12">Token</TableHeadCell>
           <TableHeadCell className="w-1/12">Recipient</TableHeadCell>
         </tr>
       </thead>
