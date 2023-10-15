cd505549e6ed0f6a688612eded51245d9146df7b
roman.rolgin <irolgin@gmail.com>
2023-10-15 18:14:20 +0400

    feat: move styles higher

--- src/components/wallet/WalletProvider.tsx
+++ src/components/wallet/WalletProvider.tsx
@@ -1,10 +1,10 @@
+import "@solana/wallet-adapter-react-ui/styles.css";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import {
   ConnectionProvider,
   WalletProvider as SolanaWalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import "@solana/wallet-adapter-react-ui/styles.css";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { clusterApiUrl } from "@solana/web3.js";
 import { PropsWithChildren, useMemo } from "react";
